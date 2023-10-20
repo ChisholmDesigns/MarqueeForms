27ff7d27963939b832b82bff3aa5e5e2f3a92621
ChisholmDesigns <148490439+ChisholmDesigns@users.noreply.github.com>
2023-10-19 20:03:42 -0400

    Update marqueeforms.js

--- marqueeforms.js
+++ marqueeforms.js
@@ -162,16 +162,16 @@ $('.select-item').each(function(){
 
     tinymce.init({
         selector: '#event-frontpage',
-        menu: {
+           menu: {
             edit: { title: 'Edit', items: 'undo redo | cut copy paste | selectall' },
             format: { title: 'Format', items: 'bold italic underline | removeformat' },
             tools: { title: 'Tools', items: 'spellchecker spellcheckerlanguage | wordcount' },
         },
         plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount',
-       toolbar: 'undo redo | blocks | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+       toolbar: 'undo redo | blocks | bold italic underline strikethrough | link image media | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
         toolbar_mode: 'floating',
-	         setup: function (editor) {
-        editor.on('init', function () {
+        content_style: "body::before { content: 'Shakespeare Birthplace Trust\nThe Old Globe'; font-size: 14px; color: #8687a8; }",
+        setup: function (editor) { editor.on('init', function () {
             var content = 'Written by <strong>William Shakespeare</strong>';
             editor.setContent(content);
         });
