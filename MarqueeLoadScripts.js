2162adea22386298ee254f7d888c05b187cbb691
ChisholmDesigns <148490439+ChisholmDesigns@users.noreply.github.com>
2024-02-13 01:13:02 -0500

    Update MarqueeLoadScripts.js

--- MarqueeLoadScripts.js
+++ MarqueeLoadScripts.js
@@ -9,8 +9,24 @@ const scriptSources = [
   { src: "https://cdn.jsdelivr.net/npm/@finsweet/attributes-smartlightbox@1/smartlightbox.js", defer: true },
   { src: "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsselect@1/cmsselect.js", async: true },
   { src: "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmssort@1/cmssort.js", async: true },
-  { src: "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsload@1/cmsload.js", async: true }
+  { src: "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsload@1/cmsload.js", async: true },
+  { src: "https://cdn.jsdelivr.net/npm/@splidejs/splide@4.0.7/dist/js/splide.min.js", async: true },
+  { src: "https://unpkg.com/popper.js@1", async: true },
+  { src: "https://unpkg.com/tippy.js@4", async: true }
 ];
+
+window.addEventListener('load', function() {
+    setTimeout(function() {
+      var sortButton = document.getElementById('fs_cmssort_button-1');
+      if (sortButton) sortButton.click();
+    }, 1000); // Adjust the delay as needed, here it's 1000 milliseconds (1 second)
+  });
+
+  document.getElementById('refreshButton').addEventListener('click', function() {
+    var uniqueToken = new Date().getTime();
+    var newUrl = window.location.href.split('?')[0] + '?cacheBust=' + uniqueToken;
+    window.location.href = newUrl;
+  });
 
 const stylesheetHref = "https://fengyuanchen.github.io/datepicker/css/datepicker.css";
 
