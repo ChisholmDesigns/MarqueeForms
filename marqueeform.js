db833002de76c686be51ddcebe1af69cb2ea98cf
ChisholmDesigns <148490439+ChisholmDesigns@users.noreply.github.com>
2024-02-22 14:21:44 -0500

    Update marqueeform.js

--- marqueeform.js
+++ marqueeform.js
@@ -1,16 +1,3 @@
- $(document).ready(function () {
-        $('[data-toggle="datepicker"]').datepicker({
-            format: 'mm-dd-yyyy'
-        });
-        // Available date placeholders:
-        // Year: yyyy
-        // Month: mm
-        // Day: dd
-        if (window.innerWidth < 768) {
-            $('[data-toggle="datepicker"]').attr('readonly', 'readonly')
-        }
-    });
-
 document.addEventListener("DOMContentLoaded", function () {
 
     var splide = new Splide(".splide", {
